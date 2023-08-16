f0e449c0297c48a8eb4634d37c5d7c3857137bf6
Danielsilva200319 <silvaguerrerodanielstiven@gmail.com>
2023-08-16 13:22:44 -0500

    a

---
+++ retos.js
@@ -0,0 +1,9 @@
+// Reto 1
+
+    function contraseña(){
+        let preg = prompt("¿Cuál es tu contraseña?:");
+        if (preg === "12345"){
+            console.log("Contraseña Correcta.")
+        }
+    }
+    contraseña();
